48741784b2fa7d4e730f695b13e5dea2dbff43b2
afnan <wansari964@yahoo.com>
2023-03-25 22:05:34 +0530

    adde ignore filer

--- Basics/src/basic.java
+++ Basics/src/basic.java
@@ -1,5 +1,33 @@
+import java.util.Scanner;
 public class basic{
     public static void main(String args[]){
-        System.out.println("Hey");
+        Scanner sc = new Scanner(System.in);
+        int m1[][] = new int[3][3];
+        int m2[][] = new int[3][3];
+        int res[][] = new int[3][3];
+
+        for(int i=0;i<3;i++){
+            for(int j=0;j<3;j++){
+                m1[i][j] = sc.nextInt();
+            }
+        }
+
+        for(int i=0;i<3;i++){
+            for(int j=0;j<3;j++){
+                m2[i][j] = sc.nextInt();
+            }
+        }
+        for(int i=0;i<3;i++){
+            for(int j=0;j<3;j++){
+                res[i][j] = m1[i][j] + m2[i][j];
+            }
+        }
+
+        for(int i=0;i<3;i++){
+            for(int j=0;j<3;j++){
+                System.out.print(res[i][j] + "\t");
+            }
+            System.out.println();
+        }
     }
 }
